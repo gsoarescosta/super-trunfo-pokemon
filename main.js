2a7db07d1138f3576b7e29af612d6b7e90887f52
Gabriel Soares Costa <gabrielsoarescosta.gsc@gmail.com>
2021-09-23 15:54:48 -0300

    Changed background, developed game logics, updates names from pt-br to english

--- main.js
+++ main.js
@@ -1,89 +1,128 @@
-var carta1 = {
-  nome: "Bulbasaur",
-  atributos: {
-    ataque: 4,
-    defesa: 8,
-    hp: 7,
-  },
-};
-
-var carta2 = {
-  nome: "Squirtle",
-  atributos: {
-    ataque: 5,
-    defesa: 6,
-    hp: 8,
-  },
-};
-
-var carta3 = {
-  nome: "Charmander",
-  atributos: {
-    ataque: 8,
-    defesa: 5,
+var pikachu = {
+  name: "Pikachu",
+  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+  attr: {
+    atk: 7,
+    def: 6,
     hp: 6,
   },
 };
 
-var cartas = [carta1, carta2, carta3];
-var cartaMaquina;
-var cartaJogador;
+var bulbasaur = {
+  name: "Bulbasaur",
+  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+  attr: {
+    atk: 4,
+    def: 8,
+    hp: 7,
+  },
+};
 
-// Alterar cartas para API https://pokeapi.co/
+var squirtle = {
+  name: "Squirtle",
+  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+  attr: {
+    atk: 5,
+    def: 7,
+    hp: 7,
+  },
+};
 
-function sortearCarta() {
-  var numeroCartaMaquina = parseInt(Math.random() * 3);
-  cartaMaquina = cartas[numeroCartaMaquina];
+var charmander = {
+  name: "Charmander",
+  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+  attr: {
+    atk: 8,
+    def: 6,
+    hp: 5,
+  },
+};
 
-  var numeroCartaJogador = parseInt(Math.random() * 3);
-  while (numeroCartaMaquina == numeroCartaJogador) {
-    numeroCartaJogador = parseInt(Math.random() * 3);
+var npcCard;
+var playerCard;
+var cards = [pikachu, bulbasaur, squirtle, charmander];
+
+function getCard() {
+  var npcCardNumber = parseInt(Math.random() * cards.length);
+  npcCard = cards[npcCardNumber];
+  var playerCardNumber = parseInt(Math.random() * cards.length);
+  while (playerCardNumber == npcCardNumber) {
+    playerCardNumber = parseInt(Math.random() * cards.length);
   }
-  cartaJogador = cartas[numeroCartaJogador];
-  console.log(cartaJogador);
-
-  document.getElementById("btnSortear").disabled = true;
-  document.getElementById("btnJogar").disabled = false;
-
-  exibirOpcoes();
+  playerCard = cards[playerCardNumber];
+  document.getElementById("button-get-card").disabled = true;
+  document.getElementById("button-play").disabled = false;
+  showPlayerCard();
 }
 
-function exibirOpcoes() {
-  var opcoes = document.getElementById("opcoes");
-  var opcoesTexto = "";
-
-  for (var atributo in cartaJogador.atributos) {
-    opcoesTexto +=
-      "<input type='radio' name='atributo' value='" +
-      atributo +
-      "'>" +
-      atributo;
-  }
-  opcoes.innerHTML = opcoesTexto;
-}
-
-function obtemAtributoSelecionado() {
-  var radioAtributos = document.getElementsByName("atributo");
-
-  for (var i = 0; i < radioAtributos.length; i++) {
-    if (radioAtributos[i].checked == true) {
-      return radioAtributos[i].value;
+function getAttr() {
+  var radioAttr = document.getElementsByName("attr");
+  for (var i = 0; i < radioAttr.length; i++) {
+    if (radioAttr[i].checked) {
+      return radioAttr[i].value;
     }
   }
 }
 
-function jogar() {
-  var atributoSelecionado = obtemAtributoSelecionado();
-  var elementoResultado = document.getElementById("resultado");
-  var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
-  var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+function play() {
+  var sameAttr = getAttr();
+  var divResult = document.getElementById("result");
 
-  if (valorCartaJogador > valorCartaMaquina) {
-    elementoResultado.innerHTML = "Você venceu";
-  } else if (valorCartaMaquina > valorCartaJogador) {
-    elementoResultado.innerHTML = "Você perdeu, a carta da máquina é maior";
+  if (playerCard.attr[sameAttr] > npcCard.attr[sameAttr]) {
+    resultTextHTML = "<p class='result'>Venceu</p>";
+  } else if (playerCard.attr[sameAttr] < npcCard.attr[sameAttr]) {
+    resultTextHTML = "<p class='result'>Perdeu</p>";
   } else {
-    elementoResultado.innerHTML = "Empatou";
+    resultTextHTML = "<p class='result'>Empatou</p>";
   }
-  console.log(cartaMaquina);
+  divResult.innerHTML = resultTextHTML;
+
+  document.getElementById("button-play").disabled = true;
+  showNpcCard();
+}
+
+function showPlayerCard() {
+  var divPlayerCard = document.getElementById("player-card");
+  divPlayerCard.style.backgroundImage = `url(${playerCard.image})`;
+  var frame =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" class="card-frame">';
+  var tagHTML = "<div id='options' class='card-status'>";
+
+  var optionsText = "";
+  for (var attr in playerCard.attr) {
+    optionsText +=
+      "<input type='radio' name='attr' value='" +
+      attr +
+      "'>" +
+      attr +
+      " " +
+      playerCard.attr[attr] +
+      "<br>";
+  }
+  var name = `<p class="card-subtitle">${playerCard.name}</p>`;
+
+  divPlayerCard.innerHTML = frame + name + tagHTML + optionsText + "</div>";
+}
+
+function showNpcCard() {
+  var divNpcCard = document.getElementById("npc-card");
+  divNpcCard.style.backgroundImage = `url(${npcCard.image})`;
+  var frame =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" class="card-frame">';
+  var tagHTML = "<div id='options' class='card-status'>";
+
+  var optionsText = "";
+  for (var attr in npcCard.attr) {
+    optionsText +=
+      "<p type='text' name='attr' value='" +
+      attr +
+      "'>" +
+      attr +
+      " " +
+      npcCard.attr[attr] +
+      "</p>";
+  }
+  var name = `<p class="card-subtitle">${npcCard.name}</p>`;
+
+  divNpcCard.innerHTML = frame + name + tagHTML + optionsText + "</div>";
 }
