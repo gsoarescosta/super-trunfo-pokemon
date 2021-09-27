0a4b214519f303f15e12c14961ac2d943c3e0433
Gabriel Soares Costa <gabrielsoarescosta.gsc@gmail.com>
2021-09-26 22:20:53 -0300

    Separating JS files

--- main.js
+++ main.js
@@ -1,46 +1,5 @@
-var pikachu = {
-  name: "Pikachu",
-  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-  attr: {
-    atk: 7,
-    def: 6,
-    hp: 6,
-  },
-};
-
-var bulbasaur = {
-  name: "Bulbasaur",
-  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-  attr: {
-    atk: 4,
-    def: 8,
-    hp: 7,
-  },
-};
-
-var squirtle = {
-  name: "Squirtle",
-  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
-  attr: {
-    atk: 5,
-    def: 7,
-    hp: 7,
-  },
-};
-
-var charmander = {
-  name: "Charmander",
-  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-  attr: {
-    atk: 8,
-    def: 6,
-    hp: 5,
-  },
-};
-
 var npcCard;
 var playerCard;
-var cards = [pikachu, bulbasaur, squirtle, charmander];
 
 function getCard() {
   var npcCardNumber = parseInt(Math.random() * cards.length);
